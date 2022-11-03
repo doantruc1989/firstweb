180dfa651d3a5de9eab0a5a8d415091d130e0138
doantruc <doantruc1989@gmail.com>
2022-11-03 11:27:39 +0700

    i got it

--- nodemailer/index.js
+++ nodemailer/index.js
@@ -1,45 +1,48 @@
 const express = require('express')
 const app = express()
 const port = 3000
+var path = require('path');
 const nodemailer = require("nodemailer");
 const { OAuth2Client } = require('google-auth-library');
+var bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 app.use(express.json())
-/**
- * Những biến sau trong thực tế nên đưa vào biến môi trường ENV vì mục đích bảo mật hơn.
- * Các bạn có thể tham khảo khóa Lập Trình MERN Stack nâng cao trên kênh YouTube:
- *  Trungquandev Official của mình để học cách triển khai & tổ chức code như đi làm thực tế nhé.
- * Link: https://www.youtube.com/c/TrungquandevOfficial/
- */
+
 const APP_PORT = 3000
 const APP_HOST = 'localhost'
 const GOOGLE_MAILER_CLIENT_ID = '49503388661-df69vuns5r99pl1hlsn1k05gejck6gof.apps.googleusercontent.com'
 const GOOGLE_MAILER_CLIENT_SECRET = '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'
-const GOOGLE_MAILER_REFRESH_TOKEN = '1//04zYF-a7wU5ckCgYIARAAGAQSNwF-L9IrN8u4xBQUW-Tz2b9IrRsmo7Acl6Kf73f57yHAfeeRquPA9PUFsTpXJYkfOrn0A1NTVHE'
+const GOOGLE_MAILER_REFRESH_TOKEN = '1//04NV78HwwS2buCgYIARAAGAQSNwF-L9Ir4xcPRspjBmSv_9iC6v7WTN9cX5P9d_MyJABqv4i1NjykvTAIbn3TuJLJiWWfEZZt4nA'
 const ADMIN_EMAIL_ADDRESS = 'ma.quy1987@gmail.com'
-// Khởi tạo OAuth2Client với Client ID và Client Secret 
+
 const myOAuth2Client = new OAuth2Client(
     GOOGLE_MAILER_CLIENT_ID,
     GOOGLE_MAILER_CLIENT_SECRET
 )
-// Set Refresh Token vào OAuth2Client Credentials
 myOAuth2Client.setCredentials({
     refresh_token: GOOGLE_MAILER_REFRESH_TOKEN
 })
-// Tạo API /email/send với method POST
+
+app.get('/', (req, res) => {
+    var options = {
+        root: path.join(__dirname),
+    };
+    var fileName = "./sendEmail.html";
+    res.sendFile(fileName, options);
+})
+
 app.post('/email/send', async (req, res) => {
+    console.log(req.body)
     try {
-        // Lấy thông tin gửi lên từ client qua body
-        const { email, subject, content } = req.body
+        const email = req.body.email;
+        const subject = req.body.subject;
+        const content = req.body.content;
+
         if (!email || !subject || !content) throw new Error('Please provide email, subject and content!')
-        /**
-         * Lấy AccessToken từ RefreshToken (bởi vì Access Token cứ một khoảng thời gian ngắn sẽ bị hết hạn)
-         * Vì vậy mỗi lần sử dụng Access Token, chúng ta sẽ generate ra một thằng mới là chắc chắn nhất.
-         */
         const myAccessTokenObject = await myOAuth2Client.getAccessToken()
-        // Access Token sẽ nằm trong property 'token' trong Object mà chúng ta vừa get được ở trên
         const myAccessToken = myAccessTokenObject?.token
-        // Tạo một biến Transport từ Nodemailer với đầy đủ cấu hình, dùng để gọi hành động gửi mail
         const transport = nodemailer.createTransport({
             service: 'gmail',
             auth: {
@@ -51,21 +54,22 @@ app.post('/email/send', async (req, res) => {
                 accessToken: myAccessToken
             }
         })
-        // mailOption là những thông tin gửi từ phía client lên thông qua API
         const mailOptions = {
-            to: email, // Gửi đến ai?
-            subject: subject, // Tiêu đề email
-            html: `<h3>${content}</h3>` // Nội dung email
+            to: email,
+            subject: subject,
+            html: `<h3>${content}</h3>`
         }
-        // Gọi hành động gửi email
         await transport.sendMail(mailOptions)
-        // Không có lỗi gì thì trả về success
+        console.log("Email sent successfully.")
         res.status(200).json({ message: 'Email sent successfully.' })
+
     } catch (error) {
-        // Có lỗi thì các bạn log ở đây cũng như gửi message lỗi về phía client
         console.log(error)
         res.status(500).json({ errors: error.message })
     }
+
 })
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => {
+    console.log(`I'm running at ${port}`)
+})
